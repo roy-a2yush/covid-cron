54e9ad19dc789fe82ef157a3327dbb53290609bf
roy-a2yush <roy.a2yush@gmail.com>
2021-05-02 16:04:36 +0530

    check

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const cron = require('node-cron')
 const shell = require('shelljs')
+const express = require('express')
+const app = express()
+app.use(express.json())
 
-cron.schedule('0 0 * * *', function() {
-    console.log('resetEmailCron running...')
-    if(shell.exec("node sayHello.js").code !== 0) {
-        console.log("Something went wrong")
-    }
-})
+const PORT = process.env.PORT || 6000
+
+const server = app.listen(
+    PORT,
+    cron.schedule('* * * * *', function() {
+        console.log('resetEmailCron running...')
+        if(shell.exec("node sayHello.js").code !== 0) {
+            console.log("Something went wrong")
+        }
+    })
+  )
